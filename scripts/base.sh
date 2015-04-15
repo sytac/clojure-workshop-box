f431685511694a53ee9313f6ffed4a51a1bfe87e
Carlo Sciolla <carlo.sciolla@klm.com>
2015-04-15 12:56:31 +0200

    Using apt-get to install the desktop env

--- scripts/base.sh
+++ scripts/base.sh
@@ -3,9 +3,7 @@ apt-get -y update
 apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
 apt-get -y install curl unzip
-
-# Install the ubuntu desktop
-tasksel install -n ubuntu-desktop
+apt-get -y install ubuntu-desktop
 
 # Set up sudo
 echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
