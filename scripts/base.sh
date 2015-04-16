5ab60f1c93f2f8fd096ac693532337dc4b34fbf2
Carlo Sciolla <carlo.sciolla@klm.com>
2015-04-16 10:53:44 +0200

    Uncommitted changes from yesterday -- git repo fuckup

--- scripts/base.sh
+++ scripts/base.sh
@@ -3,7 +3,7 @@ apt-get -y update
 apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
 apt-get -y install curl unzip
-apt-get -y install --no-install-recommends ubuntu-desktop
+apt-get -y install --no-install-recommends ubuntu-desktop gdm gnome-terminal
 
 # Set up sudo
 echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
